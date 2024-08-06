62cab610e746da14cfcc68f96057a65ba2c73e4f
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-08-06 20:09:35 +0300

    add palindrome problem solution

---
+++ src/Problems/Palindrome9.kt
@@ -0,0 +1,18 @@
+package Problems
+//Link -> https://leetcode.com/problems/palindrome-number/description/
+fun main() {
+    println(isPalindrome(121)) //true
+    println(isPalindrome(-121)) //false
+    println(isPalindrome(10)) //false
+}
+
+fun isPalindrome(x: Int): Boolean {
+    if (x < 0) return false
+    var original = x
+    var reversed= 0;
+    while (original !=0 ){
+        reversed= reversed *10 + original % 10
+        original /= 10
+    }
+    return x == reversed
+}
