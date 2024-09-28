73ce4ced7494b1d548011d4a1024e5553918f0ee
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-09-28 15:57:44 +0300

    add missing number problems solutions in leetcode

---
+++ src/problems/easy/MissingNumber.java
@@ -0,0 +1,16 @@
+package problems.easy;
+
+//Problem Linki -> https://leetcode.com/problems/missing-number/description/
+
+public class MissingNumber {
+
+    public static int missingNumber(int[] nums) {
+        int numsLength =nums.length;
+        int sum= (numsLength*(numsLength+1)) / 2;
+        int arrSum=0;
+        for(int i = 0; i<numsLength ; i++){
+            arrSum += nums[i];
+        }
+        return sum - arrSum;
+    }
+}
