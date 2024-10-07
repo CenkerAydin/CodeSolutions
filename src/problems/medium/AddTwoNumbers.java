5c93287624c23d2b190aa4cd895c404c7b3b386f
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-10-07 22:32:11 +0300

    add Add Two Numbers problems solutions in leetcode

---
+++ src/problems/medium/AddTwoNumbers.java
@@ -0,0 +1,39 @@
+package problems.medium;
+
+//Problem Linki -> https://leetcode.com/problems/add-two-numbers/
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+            int sum = val1 + val2 + carry;
+
+            carry = sum / 10;
+            int digit = sum % 10;
+
+            current.next = new ListNode(digit);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return head.next;
+    }
+}
