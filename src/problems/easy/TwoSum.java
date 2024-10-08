d67b621cc48fbb4796509e7ca504ea61a482061c
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-10-08 17:44:37 +0300

    add two sum problems solutions in leetcode

---
+++ src/problems/easy/TwoSum.java
@@ -0,0 +1,27 @@
+package problems.easy;
+
+//Problem Linki -> https://leetcode.com/problems/two-sum/description/
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for(int i=0; i< nums.length-1;i++){
+            for(int j=i+1 ;j <nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TwoSum ts = new TwoSum();
+        int[] result = ts.twoSum(new int[]{2, 7, 11, 15}, 17);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+}
