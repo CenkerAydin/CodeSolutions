89a428932206ced9eddceb767f262923a2b6c14c
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-10-08 17:35:45 +0300

    add valid anagram problems solutions in leetcode

---
+++ src/problems/easy/ValidAnagram.java
@@ -0,0 +1,22 @@
+package problems.easy;
+
+//Problem Linki -> https://leetcode.com/problems/valid-anagram/description/
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+
+        String normalizedStr1 = s.replaceAll("\\s", "").toLowerCase();
+        String normalizedStr2 = t.replaceAll("\\s", "").toLowerCase();
+
+        char[] charArray1 = normalizedStr1.toCharArray();
+        char[] charArray2 = normalizedStr2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
